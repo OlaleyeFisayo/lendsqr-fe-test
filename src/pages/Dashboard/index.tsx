1fd148ed6188e15d0e9ac67cad39eb769c50d2b9
OlaleyeFisayo <91983317+OlaleyeFisayo@users.noreply.github.com>
2023-05-26 19:32:59 +0100

    Update index.tsx

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,15 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import SideBar from "./components/Sidebar";
 
 export default function Dashboard() {
     return (
         <>
             <Navbar />
+            <div className="outlet">
+                <SideBar />
+                <Outlet />
+            </div>
         </>
     )
 }
