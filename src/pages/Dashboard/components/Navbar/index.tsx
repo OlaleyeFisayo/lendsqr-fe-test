f1e9f3bded962699aceafed9ea6cc18b1d0f41eb
OlaleyeFisayo <91983317+OlaleyeFisayo@users.noreply.github.com>
2023-05-26 15:14:48 +0100

    Updated Navbar

--- src/pages/Dashboard/components/Navbar/index.tsx
+++ src/pages/Dashboard/components/Navbar/index.tsx
@@ -7,32 +7,64 @@ import dropdown from "../../../../assets/svg/dropdown.svg";
 
 export default function Navbar() {
   return (
-    <nav>
-      <div className="img-container">
-        <img src={logo} alt="logo" />
-      </div>
-      <div className="search">
-        <input
-          type="text"
-          name="search"
-          id="search"
-          placeholder="Search for anything"
-        />
-        <div className="search_button">
-          <img src={search} alt="search icon" />
+    <>
+      <nav className="desktop-view">
+        <div className="logo-container">
+          <img src={logo} alt="logo" />
         </div>
-      </div>
-      <div className="details">
-        <p>Docs</p>
-        <img src={bell} alt="notification bell" />
-        <div className="user-details">
-          <div className="user-img">
-            <img src={profileImg} alt="user image" />
+        <div className="search">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search for anything"
+          />
+          <div className="search_button">
+            <img src={search} alt="search icon" />
           </div>
-          <p className="username">Fisayo</p>
-          <img src={dropdown} alt="" />
         </div>
-      </div>
-    </nav>
+        <div className="details">
+          <p className="docs">Docs</p>
+          <img src={bell} alt="notification bell" />
+          <div className="user-details">
+            <div className="user-img">
+              <img src={profileImg} alt="user image" />
+            </div>
+            <p className="username">Fisayo</p>
+            <img src={dropdown} alt="dropdown" />
+          </div>
+        </div>
+        <div className="hamburger">
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+      </nav>
+
+      <nav className="mobile-view">
+        <div className="search">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            placeholder="Search for anything"
+          />
+          <div className="search_button">
+            <img src={search} alt="search icon" />
+          </div>
+        </div>
+        <div className="details">
+          <p className="docs">Docs</p>
+          <img src={bell} alt="notification bell" />
+          <div className="user-details">
+            <div className="user-img">
+              <img src={profileImg} alt="user image" />
+            </div>
+            <p className="username">Fisayo</p>
+            <img src={dropdown} alt="dropdown" />
+          </div>
+        </div>
+      </nav>
+    </>
   );
 }
