46141862903596e76fbfbf08c0d152e00f618fa2
OlaleyeFisayo <91983317+OlaleyeFisayo@users.noreply.github.com>
2023-05-28 12:52:39 +0100

    Adding the User section

--- src/pages/Dashboard/Outlets/User/index.tsx
+++ src/pages/Dashboard/Outlets/User/index.tsx
@@ -1,5 +1,17 @@
+import "./index.scss";
+
 export default function User() {
-    return (
-        <></>
-    )
+  return (
+    <section className="user">
+      <div className="user-content">
+        <h1>Users</h1>
+        <div>
+          <div className="users"></div>
+          <div className="users"></div>
+          <div className="users"></div>
+          <div className="users"></div>
+        </div>
+      </div>
+    </section>
+  );
 }
