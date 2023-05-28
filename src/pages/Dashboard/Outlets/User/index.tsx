036d1dbfef20c971d7e6edc33660fa3e83bca888
OlaleyeFisayo <91983317+OlaleyeFisayo@users.noreply.github.com>
2023-05-28 13:00:51 +0100

    Updated User section

--- src/pages/Dashboard/Outlets/User/index.tsx
+++ src/pages/Dashboard/Outlets/User/index.tsx
@@ -1,15 +1,35 @@
 import "./index.scss";
+import ellipseOne from "../../../../assets/svg/ellipse1.svg"
+import ellipseTwo from "../../../../assets/svg/ellipse2.svg"
+import ellipseThree from "../../../../assets/svg/ellipse3.svg"
+import ellipseFour from "../../../../assets/svg/ellipse4.svg"
 
 export default function User() {
   return (
     <section className="user">
       <div className="user-content">
         <h1>Users</h1>
-        <div>
-          <div className="users"></div>
-          <div className="users"></div>
-          <div className="users"></div>
-          <div className="users"></div>
+        <div className="user-container">
+          <div className="users">
+            <img src={ellipseOne} alt="user" />
+            <h1>users</h1>
+            <p>2,453</p>
+          </div>
+          <div className="users">
+            <img src={ellipseTwo} alt="active users" />
+            <h1>active users</h1>
+            <p>2,453</p>
+          </div>
+          <div className="users">
+            <img src={ellipseThree} alt="users with loans" />
+            <h1>users</h1>
+            <p>12,453</p>
+          </div>
+          <div className="users">
+            <img src={ellipseFour} alt="users with savings" />
+            <h1>users</h1>
+            <p>102,453</p>
+          </div>
         </div>
       </div>
     </section>
