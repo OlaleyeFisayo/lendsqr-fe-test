dbacec50aab6fcbf45c1f70b5256406b7b5b0316
OlaleyeFisayo <91983317+OlaleyeFisayo@users.noreply.github.com>
2023-05-28 13:53:48 +0100

    Updated Hooks folder

--- src/pages/Login/components/Form/index.tsx
+++ src/pages/Login/components/Form/index.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 import { useContext, useRef } from "react";
 import "./index.scss";
 import { AppContext } from "../../../../setup/app-context-manager";
-import { useToggleButton } from "../../../../setup/hooks";
+import { useToggleButton } from "../../../../setup/hooks/useToggleButton";
 
 export default function Form() {
   const { toggleButton, handleClick, handleSubmit } = useContext(AppContext);
